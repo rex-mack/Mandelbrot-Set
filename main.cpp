b4dee3b0bdf9dad62c87989ead5cf0d374963317
Rex Mack <rexmack@Rexs-MacBook-Pro.local>
2025-07-09 20:57:00 -0700

    start on graph

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 int main() {
-    cout << setprecision(10) << fixed;
-
     int imageSize[2] = {IMAGE_WIDTH,IMAGE_HEIGHT};
     int numberIterations = 1;
     long double zoom = 1;
-    Complex center(0.0,0.0);
-    
+    Complex center(0,0);
+
+
+    cout << setprecision(10) << fixed; //change this to be dependent on zoom 
     cout << "===== Mandelbrot Set Generator =====" << endl;
     cout << endl;
     cout << "Image Size: (" << imageSize[0] << ", " << imageSize[1] << ")" << endl;
@@ -20,9 +20,30 @@ int main() {
     cout << "Zoom: " << zoom << endl;
     cout << endl;
     cout << "------------------------------------" << endl;
-    cout << endl;
-    cout << "Pixel Size: " << endl;
-    cout << endl;
-    cout << "====================================" << endl;
+
+    //create graph
+    Complex graph[imageSize[0]][imageSize[1]];
+
+    //apply coordinates
+    /*
+    at least have (-5,5) and (5, -5) coordinates in zoom 1, center(0,0)
+    */
+
+    //get distance between each pixel
+    int fracNumer = 5;
+    int fracDenom = (imageSize[0]>imageSize[1]) ? imageSize[1] : imageSize[0];
+
+    
+
+
+    for (int posX = 0; posX < imageSize[0]; posX++) {
+        for (int posY = 0; posY < imageSize[1]; posY++) {
+            graph[posX][posY] = Complex();
+        }
+    }
+    //do mandelbrot recursion on number
+
+    //apply color
+    //print picture
 
 }
